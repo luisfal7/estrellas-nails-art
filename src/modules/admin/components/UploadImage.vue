a68aa67b59eb6e70cf85e6b93887fb0b1d6c6e7d
Luis Falconi <luism.falconi@gmail.com>
2022-12-09 12:41:13 -0300

    modif name helpers admin, and modif admin/comp/uploadImage

--- src/modules/admin/components/UploadImage.vue
+++ src/modules/admin/components/UploadImage.vue
@@ -1,72 +1,76 @@
 <template>
   <h1>Cargar imagenes</h1>
+  <img
+    v-if="localImage"
+    :src="localImage"
+    alt="picture" 
+    />
   <div>
-    <img
-      :src="localImage"
-      alt="picture"
-    />
     <input
-        ref="imageSelector"
-        type="file"
-        @change="onSelectedImage"
-        v-show="false"
-        accept="image/png, image/jpeg"
+      ref="imageSelector"
+      type="file"
+      @change="onSelectedImage"
+      v-show="false"
+      accept="image/png, image/jpeg"
     />
-  <button @click="onSelectImage">seleccionar imagen</button>
-  <button @click="onSaveImage">guardar</button>
+    <button @click="onSelectImage">seleccionar imagen</button>
+    <button @click="onSaveImage">guardar</button>
+    <button @click="onCancelImage">cancelar</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { useStore } from 'vuex'
-import uploadImage from "@/modules/admin/helpers/uploadImage";
+import { ref } from "vue";
+import uploadImageDB from "@/modules/admin/helpers/uploadImageDB";
 
 export default {
+  setup() {
 
-    setup(){
-        const store = useStore()
+    let imageSelector = ref(null);
+    let localImage = ref(null);
+    let file = ref(null);
 
-        const imageSelector = ref(null)
-        const localImage =  ref(null)
-        const file = ref(null)
+    return {
+      imageSelector,
 
-        return{
-            imageSelector,
+      localImage,
 
-            onSelectImage: () => imageSelector.value.click(),
+      onSelectImage: () => imageSelector.value.click(),
 
-            onSelectedImage: (event) => {
-                const uploadImage = event.target.files[0]
+      onSelectedImage: (event) => {
+        const uploadImage = event.target.files[0];
 
-                if ( !uploadImage ){
-                    localImage.value = null
-                    file.value = null
-                    return    
-                }
-
-                file.value = uploadImage 
-
-                const fr = new FileReader()
-                fr.onload = () => localImage.value = fr.result
-                fr.readAsDataURL( uploadImage )
-                console.log( uploadImage )
-            },
-            onSaveImage: async() => {
-                await uploadImage( file.value )
-            }
+       if (!uploadImage) {
+          localImage.value = null
+          file.value = null
+          return;
         }
-    }
-}
+        file = uploadImage
+
+        const fr = new FileReader();
+        fr.onload = () => (localImage.value = fr.result)
+        fr.readAsDataURL(uploadImage)
+      },
+
+      onSaveImage: async () => {
+        await uploadImageDB(file);
+      },
+
+      onCancelImage: () => {
+        localImage.value = null
+        file.value = null
+      }
+    };
+  },
+
+};
 </script>
 
 <style scoped>
-
 img {
   border: 1px solid;
   width: 200px;
-  position:fixed;
+  position: fixed;
   bottom: 50px;
 }
-
 </style>
---
+++ src/modules/admin/helpers/uploadImageDB.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const uploadImageDB = async (file) => {
+
+  if( !file ) return
+
+  try {
+
+    const formData = new FormData()
+    formData.append("upload_preset", "estrellas-nails-art")
+    formData.append("file", file)
+    
+    const url = "https://api.cloudinary.com/v1_1/do7cayuwj/image/upload"
+
+    const { data } = await axios.post(url, formData)
+    
+    console.log(data.url)
+    
+    return data.url;
+
+  } catch (error) {
+
+    console.log("Error al cargar la imagen, revisar logs");
+    console.log(error);
+    return null;
+
+  }
+};
+
+export default uploadImageDB;
