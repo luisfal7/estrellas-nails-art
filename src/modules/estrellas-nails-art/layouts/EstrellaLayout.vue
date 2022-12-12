633f696e219440a0d4ca3f6961a219033f4eadf1
Luis Falconi <luism.falconi@gmail.com>
2022-12-12 13:53:43 -0300

    add nav button login

--- src/modules/estrellas-nails-art/layouts/EstrellaLayout.vue
+++ src/modules/estrellas-nails-art/layouts/EstrellaLayout.vue
@@ -1,9 +1,22 @@
 <template>
+<nav>
+  <button @click="onLogin">login</button>
+</nav>
   <router-view/>
 </template>
 
 <script>
+import { useRouter } from 'vue-router'
+
 export default {
+  setup(){
+    const router = useRouter()
+
+    return{
+      onLogin: () => router.push({ name: "login" })
+    }
+
+  }
 
 }
 </script>
