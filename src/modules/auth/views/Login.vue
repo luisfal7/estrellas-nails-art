cd2529f309aa12f395c6b5c6eef4a6b74433ff5d
Luis Falconi <luism.falconi@gmail.com>
2022-12-12 14:00:24 -0300

    edit view login

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,5 +1,11 @@
 <template>
   <h1>Login view</h1>
+  <form>
+    <input type="email" placeholder="Correo" required />
+    <input type="password" placeholder="Contraseña" required />
+    <button>Login</button>
+    <router-link :to="{ name:'register' }">¿No tienes cuenta?</router-link>
+  </form>
 </template>
 
 <script>
